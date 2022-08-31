a8ebfc8f4371d140a3c5d82160c810c84bdaf626
Dino.C <40124790+kyo22222@users.noreply.github.com>
2022-08-31 12:16:33 +0800

    Car.js update
    semi-filter part A

--- src/Car.js
+++ src/Car.js
@@ -14,20 +14,30 @@ function Car() {
     //     <tr data-title={key}><td>{data.brand}</td><td>{data.prodname_e}</td><td>{data.srp}</td></tr>
     // );
 
-    function sortAudio(){
-        const filtered = phoneArr.filter(products => {
-            return products.brand === 'Audio and Video';
-        })
+    const [catee, setCate] = useState('');
+    function clickCATE(e){
+        //const {txt} = e.target.dataset.id;
+        const txt = e.target.value;
+        //console.log(e.target.innerText);
+        setCate(txt);
     }
-    
-    //console.log(filtered);
+    const filtered = phoneArr.filter(products => {
+        if(catee !== ''){
+            return products.brand === catee;
+        }else{
+            return true;
+        }   
+    })
 
+     
     return (
-        
-        <div className="theBOX">
-            
+       
+        <div className="theBOX">            
             <Karina></Karina>
-            <button onClick={sortAudio}>Audio</button>
+           <button onClick={clickCATE} value="">ALL</button> 
+           <button onClick={clickCATE} value="Audio and Video">Audio and Video</button>
+           <button onClick={clickCATE} value="Mobile Handset">Mobile Handset</button>
+           
             <table className='table-sort' cellSpacing="0" cellPadding="0">
                 <thead><tr><th>products.brand</th><th>products.prodname_e</th><th>products.srp</th></tr></thead>
                 <tbody>
